53d38548bcf2fded9199363b4963666ef92c99a5
awalji <aliya.walji@gmail.com>
2014-08-26 14:03:58 -0400

    created scrolling pipe sprites

--- game.py
+++ game.py
@@ -5,8 +5,7 @@ import pygame
 from pygame.constants import QUIT
 from pygame.time import Clock
 from pygame import Surface, display
-from pygame.sprite import Sprite
-
+from pygame.sprite import Sprite, RenderUpdates
 
 SCREEN_RES = (640, 480)
 
@@ -16,6 +15,9 @@ TANGERINE = (255, 204, 0)
 
 PALE_GOLDENROD = (238, 232, 170)
 
+KELLY_GREEN = (76, 187, 23)
+
+PIPE_RATE = 4000
 
 pygame.init()
 
@@ -39,8 +41,6 @@ class FlappyBox(Sprite):
         self.rect.left = 60
 
 fbox = FlappyBox()
-screen.blit(fbox.image, fbox.rect)
-
 
 class Ground(Sprite):
     def __init__(self):
@@ -51,16 +51,44 @@ class Ground(Sprite):
         self.rect.bottom = 480
 
 ground = Ground()
-screen.blit(ground.image, ground.rect)
+
+class Pipe(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = Surface((60, 480))
+        self.rect = self.image.get_rect()
+        self.image.fill(KELLY_GREEN)
+        self.rect.left = 640
+
+    def update(self):
+        self.rect.left -= 1
+
+sprites = RenderUpdates(fbox, ground)
+
+pipe_timer = 0
 
 while True:
+
+    sprites.clear(screen, background)
 
     for event in pygame.event.get():
 
         if event.type == QUIT:
             sys.exit()
 
-    clock.tick(60)
+    pipe_timer += clock.tick(60)
+
+    if pipe_timer >= PIPE_RATE:
+        top_pipe = Pipe()
+        top_pipe.rect.bottom = 150
+        bottom_pipe = Pipe()
+        bottom_pipe.rect.top = 330
+        pipe_timer = 0
+        sprites.add(top_pipe, bottom_pipe)
+
+    sprites.update()
+
+    sprites.draw(screen)
 
     display.flip()
 
